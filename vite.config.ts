e90e232d9a4535d715ecb88c12c921c8a3911056
emmanuelYohore <emmanuelbasket01@gmail.com>
2026-02-17 13:39:46 +0100

    merge on main

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ui from '@nuxt/ui/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import ui from '@nuxt/ui/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
